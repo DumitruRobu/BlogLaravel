49810b9a3816764da6275e4f01461eeeb3c64a44
Dumitru.Robu <orlando001@yandex.ru>
2023-09-21 15:10:32 +0300

    lectia 5

---
+++ app/Http/Controllers/Admin/Category/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Http\Controllers\Admin\Category;
+
+
+class IndexController
+{
+    public function __invoke()
+    {
+        return view("admin.categories.index");
+    }
+}
