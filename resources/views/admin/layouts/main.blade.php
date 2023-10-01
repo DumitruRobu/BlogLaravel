3a58da90e4edebaf859e2ca77e2505eddab6e5c0
Dumitru.Robu <orlando001@yandex.ru>
2023-10-01 18:39:13 +0300

    lectia 26

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -36,14 +36,29 @@
     </div>
 
     <!-- Navbar -->
-    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light ">
         <!-- Left navbar links -->
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-            </li>
+        <div class="col-12 d-flex justify-content-between">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+                </li>
 
-        </ul>
+            </ul>
+
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <form action="{{route('logout')}}" method="POST">
+                        @csrf
+                        <input class="btn btn-outline-primary" type="submit" value="Exit">
+                    </form>
+                </li>
+
+            </ul>
+        </div>
+
+
+
     </nav>
     <!-- /.navbar -->
 
--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
 <main class="blog">
+
+
     <div class="container">
         <h1 class="edica-page-title" data-aos="fade-up">Blog</h1>
         <section class="featured-posts-section">
